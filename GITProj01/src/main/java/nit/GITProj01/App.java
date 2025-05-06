9e4e6c7cb12e3c6ce571062ef52cdb9ff510bef7
lopamudra-rout <120742637+lopamudra-rout@users.noreply.github.com>
2025-05-06 15:46:09 +0530

    task1 by dev1

--- GITProj01/src/main/java/nit/GITProj01/App.java
+++ GITProj01/src/main/java/nit/GITProj01/App.java
@@ -4,7 +4,14 @@ package nit.GITProj01;
  * Hello world!
  */
 public class App {
+	
+	public int sum(int x, int y)
+	{
+		return x+y;
+	}
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	App app=new App();
+    	
+        System.out.println("Sum is:"+app.sum(4, 4));
     }
 }
--- GITProj01/src/test/java/nit/GITProj01/AppTest.java
+++ GITProj01/src/test/java/nit/GITProj01/AppTest.java
@@ -1,5 +1,6 @@
 package nit.GITProj01;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,21 @@ public class AppTest {
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testWithPositive() {
+    	App app=new App();
+    	int expected=8;
+    	int actual=app.sum(4, 4);
+        assertEquals(expected,actual);
     }
+    
+    @Test
+    public void testWithNegative() {
+    	App app=new App();
+    	int expected=-8;
+    	int actual=app.sum(-4, -4);
+        assertEquals(expected,actual);
+    }
+    
+    
+    
 }
